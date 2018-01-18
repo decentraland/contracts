8f722e18b88bafa8a4493268848f64bf391a5ba1
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-01-18 15:20:28 -0300

    Remove master files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Abi accessible using the `.abi` property
-  LANDTerraformSale: require('./LANDTerraformSale'),
-  LANDTestSale: require('./LANDTestSale'),
-  LANDRegistry: require('./LANDRegistry'),
-  MANAToken: require('./MANAToken'),
-  TerraformReserve: require('./TerraformReserve')
-}
