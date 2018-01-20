7785a8887f4bd59547198d10204f7a3adacd2d1b
Juan Cazala <juancazala@gmail.com>
2018-01-20 19:29:42 -0300

    added DecentralandVesting to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   // Abi accessible using the `.abi` property
+  DecentralandVesting: require('./DecentralandVesting'),
   LANDTerraformSale: require('./LANDTerraformSale'),
   LANDTestSale: require('./LANDTestSale'),
   LANDRegistry: require('./LANDRegistry'),
