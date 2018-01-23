632c58a51f97cec12bb55b210d911aa3e234341f
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-01-23 15:22:22 +0100

    Remove interfaces. Check https://github.com/decentraland/commons

--- src/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Abi accessible using the `.abi` property
-  DecentralandVesting: require('./DecentralandVesting'),
-  LANDTerraformSale: require('./LANDTerraformSale'),
-  LANDTestSale: require('./LANDTestSale'),
-  LANDRegistry: require('./LANDRegistry'),
-  MANAToken: require('./MANAToken'),
-  TerraformReserve: require('./TerraformReserve')
-}
